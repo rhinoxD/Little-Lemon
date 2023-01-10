1edb42744b1de8c232cab5595de89f1a87359dfd
unknown <coolshiv0721@gmail.com>
2023-01-10 11:03:12 +0530

    Setup semantic html and OGP

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import footerlogo from '../assets/new/Asset20@4x.png'
+
+const Footer = () => {
+  return (
+    <footer>
+      <article>
+        <img src={footerlogo} alt='' />
+      </article>
+      <article>
+        <p>Copyright Little Lemon</p>
+      </article>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import header from '../assets/Logo.svg'
+
+const Header = () => {
+  return (
+    <header>
+      <img src={header} alt='header-logo' />
+    </header>
+  )
+}
+
+export default Header
