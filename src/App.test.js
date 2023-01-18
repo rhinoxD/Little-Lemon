4cf78cf24cbc65dcf0d2ef21361477f042a958cc
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-18 09:55:40 +0530

    Add useless tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import BookingForm from './components/BookingForm'
+import BookingPage from './components/BookingPage'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const initialTimings = [
+  'Select a time...',
+  '17:00',
+  '18:00',
+  '19:00',
+  '20:00',
+  '21:00',
+  '22:00',
+]
+
+test('Renders the BookingForm heading', () => {
+  render(<BookingForm />)
+  const headingElement = screen.getByText('Booking Form')
+  expect(headingElement).toBeInTheDocument()
+})
+
+test('should return the initial state', () => {
+  render(<BookingPage />)
+  const availableTimes = initialTimings
+  expect(availableTimes).toEqual(initialTimings)
+})
+
+// test('returns the same value that is provided in the state', () => {
+//   render(<BookingPage />)
+//   const state = BookingPage()
+//   expect(state).toEqual(initialTimings)
+// })
--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -29,38 +29,41 @@ const BookingForm = ({ availableTimes, dispatch }) => {
     console.log(date, time, guests, occasion)
   }
   return (
-    <div className='form'>
-      <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }}>
-        <label htmlFor='res-date'>Choose date</label>
-        <input type='date' id='res-date' onChange={handleDate} value={date} />
-        <label htmlFor='res-time'>Choose time</label>
-        <select id='res-time ' onChange={handleTime}>
-          {availableTimes &&
-            availableTimes.map((avlTime) => (
-              <option key={avlTime}>{avlTime}</option>
-            ))}
-        </select>
-        <label htmlFor='guests'>Number of guests</label>
-        <input
-          type='number'
-          placeholder='1'
-          ref={ref}
-          min='1'
-          max='10'
-          id='guests'
-          onChange={handleGuests}
-        />
-        <label htmlFor='occasion'>Occasion</label>
-        <select id='occasion' onChange={handleOccasion}>
-          <option>Birthday</option>
-          <option>Anniversary</option>
-        </select>
-        <input
-          type='submit'
-          value='Make Your reservation'
-          onClick={handleSubmit}
-        />
-      </form>
+    <div className='booking-form'>
+      <h1>Booking Form</h1>
+      <div className='form'>
+        <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }}>
+          <label htmlFor='res-date'>Choose date</label>
+          <input type='date' id='res-date' onChange={handleDate} value={date} />
+          <label htmlFor='res-time'>Choose time</label>
+          <select id='res-time ' onChange={handleTime}>
+            {availableTimes &&
+              availableTimes.map((avlTime) => (
+                <option key={avlTime}>{avlTime}</option>
+              ))}
+          </select>
+          <label htmlFor='guests'>Number of guests</label>
+          <input
+            type='number'
+            placeholder='1'
+            ref={ref}
+            min='1'
+            max='10'
+            id='guests'
+            onChange={handleGuests}
+          />
+          <label htmlFor='occasion'>Occasion</label>
+          <select id='occasion' onChange={handleOccasion}>
+            <option>Birthday</option>
+            <option>Anniversary</option>
+          </select>
+          <input
+            type='submit'
+            value='Make Your reservation'
+            onClick={handleSubmit}
+          />
+        </form>
+      </div>
     </div>
   )
 }
