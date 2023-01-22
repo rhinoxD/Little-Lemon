b3b5edef785d0273ce0e8b9f3c149259cb062648
Shivam Kumar <coolshiv0721@gmail.com>
2023-01-22 13:40:30 +0530

    Update specials

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import bruschetta from '../assets/bruchetta.svg'
 import lemonDessert from '../assets/lemon dessert.jpg'
 import scooter from '../assets/scooter.svg'
 import { Link } from 'react-router-dom'
+import { Button } from '../components/Common/Button'
 
 const Main = () => {
   return (
@@ -30,87 +31,76 @@ const Main = () => {
         </div>
       </section>
 
-      <div className='specials'>
-        <div className='heading'>
-          <h1>This weeks specials!</h1>
-          <button aria-label='On Click'>Online Menu</button>
-        </div>
-        <div className='cards'>
-          <div className='one'>
-            <div className='card-image'>
-              <img src={greekSalad} alt='' />
-            </div>
-            <div className='card-heading'>
-              <p>Greek Salad</p>
-              <p>$ 12.99</p>
-            </div>
-            <div className='card-text'>
-              <p>
+      <section id='specials-section' className='container specials'>
+        <h2 className='specials-heading flex'>
+          <span>This week's specials!</span>
+          <Button variant='primary' handleClick={() => {}}>
+            Online Menu
+          </Button>
+        </h2>
+
+        <div className='grid specials-grid'>
+          <div className='specials-card'>
+            <img src={greekSalad} alt='Greek Salad' />
+            <div className='specials-content'>
+              <div className='specials-title flex'>
+                <div className='specials-name'>Greek Salad</div>
+                <div className='specials-price'>$ 12.99</div>
+              </div>
+              <div className='specials-description'>
                 The famous greek salad of scrispy lettuce, peppers, olives and
                 our Chicago style feta cheese, garnished with crunchy garlic and
                 rosemary croutons.
-              </p>
-            </div>
-            <div className='card-btn'>
-              <button aria-label='On Click'>
+              </div>
+              <Link to='/' className='specials-order-link'>
                 Order a delivery
                 <span>
                   <img src={scooter} alt='' />
                 </span>
-              </button>
+              </Link>
             </div>
           </div>
-
-          <div className='two'>
-            <div className='card-image'>
-              <img src={bruschetta} alt='' />
-            </div>
-            <div className='card-heading'>
-              <p>Bruschetta</p>
-              <p>$ 5.99</p>
-            </div>
-            <div className='card-text'>
-              <p>
+          <div className='specials-card'>
+            <img src={bruschetta} alt='Bruschetta' />
+            <div className='specials-content'>
+              <div className='specials-title flex'>
+                <div className='specials-name'>Bruschetta</div>
+                <div className='specials-price'>$ 5.99</div>
+              </div>
+              <div className='specials-description'>
                 Our Bruschetta is made from grilled bread that has been smeared
                 with garlic and seasoned with salt and olive oil.
-              </p>
-            </div>
-            <div className='card-btn'>
-              <button>
+              </div>
+              <Link to='/' className='specials-order-link'>
                 Order a delivery
                 <span>
                   <img src={scooter} alt='' />
                 </span>
-              </button>
+              </Link>
             </div>
           </div>
-
-          <div className='three'>
-            <div className='card-image'>
-              <img src={lemonDessert} alt='' />
-            </div>
-            <div className='card-heading'>
-              <p>Lemon Dessert</p>
-              <p>$ 5.00</p>
-            </div>
-            <div className='card-text'>
-              <p>
+          <div className='specials-card'>
+            <img src={lemonDessert} alt='Lemon Dessert' />
+            <div className='specials-content'>
+              <div className='specials-title flex'>
+                <div className='specials-name'>Lemon Dessert</div>
+                <div className='specials-price'>$ 5.00</div>
+              </div>
+              <div className='specials-description'>
                 This comes straight from grandma's recipe book, every last
                 ingredient has been sourced and is as authentic as can be
                 imagined.
-              </p>
-            </div>
-            <div className='card-btn'>
-              <button>
+              </div>
+              <Link to='/' className='specials-order-link'>
                 Order a delivery
                 <span>
                   <img src={scooter} alt='' />
                 </span>
-              </button>
+              </Link>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </main>
   )
 }
